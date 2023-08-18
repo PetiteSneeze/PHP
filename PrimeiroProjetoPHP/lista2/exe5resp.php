0b96235cd21f5d6cfd661fe7a060e652c53ce43c
André Cavichiolli <andrecavichiolli3@gmail.com>
2023-08-18 15:03:27 -0300

    exercicio5Lista2

---
+++ PrimeiroProjetoPHP/lista2/exe5resp.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>resposta exercicio 5 lista 2</title>
+  </head>
+  <body class="container">
+    <h1>
+  <?php
+    
+        // Calcula o fatorial
+        $numero1=$_POST["numero1"];
+        $fatorial = 1;
+        for ($i = $numero1; $i > 1; $i--){
+            $fatorial *= $i;
+        }
+
+        // Exibe o resultado do fatorial
+        echo "O fatorial de $numero1 é $fatorial.\n";
+        ?>
+    </h1>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
