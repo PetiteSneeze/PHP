11fc85e733c1d262386a767ba7c3023422ac3e22
André Cavichiolli <andrecavichiolli3@gmail.com>
2023-09-24 18:18:58 -0300

    vishhhh

---
+++ PrimeiroProjetoPHP/lista2/exe1resp.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Resultado</h1>
+        <?php
+       
+            $numero = $_POST["numero"];
+
+            if ($numero > 0) {
+                echo '<p>Valor Positivo</p>';
+            } elseif ($numero < 0) {
+                echo '<p>Valor Negativo</p>';
+            } else {
+                echo '<p>Igual a Zero</p>';
+            }
+       
+        ?>
+        
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
