10c8cd348fab31745e446839a20d0f30c6e24296
André Cavichiolli <andrecavichiolli3@gmail.com>
2023-09-25 19:17:36 -0300

    oioioio

--- PrimeiroProjetoPHP/lista5/upload/sessao/sair.php
+++ PrimeiroProjetoPHP/lista5/upload/sessao/sair.php
@@ -2,3 +2,8 @@
     session_destroy();
     header("Location:index.php");
     exit;
+
+
+    Serão computadas, no máximo, 5 horas por certificado, independente
+da carga horária especificada. Obedecendo ao limite de, no máximo,
+10 horas por dia, considerando como data-base de referência a data
