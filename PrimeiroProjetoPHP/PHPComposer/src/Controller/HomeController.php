f4ee9232dc9a92801a5f464aebe2d0faa7616201
André Cavichiolli <andrecavichiolli3@gmail.com>
2023-10-02 21:37:41 -0300

    Aula MVC

---
+++ PrimeiroProjetoPHP/PHPComposer/src/Controller/HomeController.php
@@ -0,0 +1,10 @@
+<?php
+    namespace Php\Biblioteca\Controller;
+
+    class HomeController{
+
+        public function index($params){
+            require "../src/View/Home/index.php";
+        }
+    }
+
